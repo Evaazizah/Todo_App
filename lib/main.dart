b052ba6e94d29cf594696d5c13e1cd0f8a402f8a
Eva_Azizah <effatulazizah04@gmail.com>
2025-04-30 18:46:37 +0700

    nambah kode 1

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,16 @@ import 'dart:convert';
 void main() {
   runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
